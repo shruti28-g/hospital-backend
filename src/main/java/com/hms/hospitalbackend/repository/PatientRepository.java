1c91eaffa608ddfbe809a0dd8fdd1b7a09e2bfba
Shruti <shruti.g2027@gmail.com>
2026-05-27 11:58:47 +0530

    Fix PatientRepository

--- src/main/java/com/hms/hospitalbackend/repository/PatientRepository.java
+++ src/main/java/com/hms/hospitalbackend/repository/PatientRepository.java
@@ -2,7 +2,9 @@ package com.hms.hospitalbackend.repository;
 
 import com.hms.hospitalbackend.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository
         extends JpaRepository<Patient, Long> {
 
